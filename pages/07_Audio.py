af48897db70d5e4b19b81c0ad3eda0c4e32277bf
Jett Wang <jamiesun.net@gmail.com>
2023-12-07 22:02:12 +0800

    clear code

--- pages/07_Audio.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from audio_recorder_streamlit import audio_recorder
-
-audio_bytes = audio_recorder("", icon_size="1x", pause_threshold=3.0)
-if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
